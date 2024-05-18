a21a8f2430d2c0ccdc0f09342edfc2f8ea910085
Alex <alex5402private@gmail.com>
2024-05-18 20:14:36 +0530

    done

--- script.js
+++ script.js
@@ -283,6 +283,13 @@ async function animstart(){
     points.push(point);
   }
 
+  function changeTextColor() {
+    const randomColor = '#' + Math.floor(Math.random() * 16777215).toString(16);
+    textnullBit.style.color = randomColor;
+  }
+
+  // Change color every second
+  setInterval(changeTextColor, 1000);
 
   async function anima(){
     let cursorX = window.innerWidth/2;
